1bd3ff0d757304946893cd641b116bfe4228a2e2
Eveliina Ketola <eveliina.ketola@elisanet.fi>
2025-10-22 19:38:59 +0300

    Add src folders

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohlema!");
+
+        String tekijä = "Eveliina Ketola";
+        System.out.println("Ohjelman tekijä: " + tekijä);
+
+        double luku1 = 5;
+        double luku2 = 2;
+
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku2-muuttujan arvo on " + luku2);
+        
+    }
+}
